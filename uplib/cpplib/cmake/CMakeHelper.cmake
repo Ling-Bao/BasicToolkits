a82b5689bfea6f89a589719194ca403a2201d443
InnerPeaceL <1561627068@qq.com>
2019-03-10 21:17:31 +0800

    add CPPLIB_ANN_EXECUTABLE

--- uplib/cpplib/cmake/CMakeHelper.cmake
+++ uplib/cpplib/cmake/CMakeHelper.cmake
@@ -17,3 +17,11 @@ macro(CPPLIB_ADD_LIBRARY TARGET_NAME)
     # ${ARGN} will store the list of source files passed to this function.
     add_library(${TARGET_NAME} ${ARGN})
 endmacro(CPPLIB_ADD_LIBRARY)
+
+# Replacement for the normal add_executable() command. The syntax remains the
+# same in that the first argument is the target name, and the following
+# arguments are the source files to use when building the target.
+macro(CPPLIB_ADD_EXECUTABLE TARGET_NAME)
+    # ${ARGN} will store the list of source files passed to this function.
+    add_executable(${TARGET_NAME} ${ARGN})
+endmacro(CPPLIB_ADD_EXECUTABLE)
